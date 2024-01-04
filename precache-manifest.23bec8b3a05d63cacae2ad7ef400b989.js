f2e3bb864da3d3cebc4aaa403f2a17abf0b28a57
Yuvraj Singh Rajput <knb10kagami@gmail.com>
2024-01-04 19:53:42 +0530

    Updates

---
+++ precache-manifest.23bec8b3a05d63cacae2ad7ef400b989.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a68bc146b63fbfe8af0967a31e058ee",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "06acab7bf81b2196a8fa",
+    "url": "/todo/static/js/2.6c767b75.chunk.js"
+  },
+  {
+    "revision": "a1f2009a5b79edc5d51a",
+    "url": "/todo/static/js/main.de60ff96.chunk.js"
+  },
+  {
+    "revision": "2ce150e2834949db39fc",
+    "url": "/todo/static/js/runtime-main.79062c6d.js"
+  }
+]);
